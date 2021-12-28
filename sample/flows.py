b44dae770dbd52845e8e752f0f0f8e44aa623e11
Mohamed-Sheik-Ali <mohamed.sheik.ali1127@zohomail.in>
2021-12-28 11:49:52 +0530

    first ONR

---
+++ sample/flows.py
@@ -0,0 +1,54 @@
+from viewflow import flow, frontend
+from viewflow.base import this, Flow
+from viewflow.flow.views import CreateProcessView, UpdateProcessView
+
+from .models import HelloWorldProcess
+
+
+@frontend.register
+class HelloWorldFlow(Flow):
+    process_class = HelloWorldProcess
+
+    draft = (
+        flow.Start(
+            CreateProcessView,
+            fields=["text"]
+        ).Permission(
+            auto_create=True
+        ).Next(this.awaiting_sign_A)
+    )
+
+    awaiting_sign_A = (
+        flow.View(
+            UpdateProcessView,
+            fields=["approved"]
+        ).Permission(
+            auto_create=True,
+        ).Next(this.awaiting_sign_B)
+    )
+
+    awaiting_sign_B = (
+        flow.View(
+            UpdateProcessView,
+            fields=["approved"]
+        ).Permission(
+            auto_create=True,
+        ).Next(this.awaiting_sign_C)
+    )
+
+    awaiting_sign_C = (
+        flow.If(lambda activation: activation.process.approved)
+        .Then(this.send)
+        .Else(this.end)
+    )
+
+    send = (
+        flow.Handler(
+            this.send_hello_world_request
+        ).Next(this.end)
+    )
+
+    end = flow.End()
+
+    def send_hello_world_request(self, activation):
+        print(activation.process.text)
